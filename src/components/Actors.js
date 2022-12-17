2705a06c87e32ef649ddf782b2bf901169700c03
Pelin Karakoca <karakocapelin@gmail.com>
2022-12-17 22:45:07 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorPage = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>{actor.name}</h2>
+      <p>Movies : </p>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}> {movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <>
+      <h1> Actors Page </h1> {actorPage}
+    </>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorPage = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <p>Movies : </p>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}> {movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <>
+      {" "}
+      <h1>Directors Page </h1> {directorPage}{" "}
+    </>
+  );
 }
 
 export default Directors;
